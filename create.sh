f015db55fea89cc6e46460657e5c832dc928bb4d
Kapil Sreedharan <kapilsreed@gmail.com>
2020-08-12 20:48:38 +0000

    Update create and delete

--- create.sh
+++ create.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-# This is a comment!
-echo "CREATE: ${1}"
+# This is a cdomment!
+export GOOGLE_APPLICATION_CREDENTIALS=/home/bladeaico/security/blade-ai-282114-aedee4bdb9d8.json
+echo "creating k8s cluster"
+export workspace=/home/bladeaico/blai/k8s
+echo "Creating terraform cluster: $(terraform init $workspace)"
+echo "Applying  terraform $(terraform apply -auto-approve  $workspace)"
--- delete.sh
+++ delete.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # This is a comment!
-echo "DELETE id: ${1}"
+echo "DELETE Cluster"
+export GOOGLE_APPLICATION_CREDENTIALS=/home/bladeaico/security/blade-ai-282114-aedee4bdb9d8.json
+export workspace=/home/bladeaico/blai/k8s
+echo "Deleting cluster: $(terraform destroy -auto-approve $workspace)"
